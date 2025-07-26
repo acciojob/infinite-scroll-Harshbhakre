60df7736441cc0ef6ff756cfe1aa08c60264035a
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-26 11:40:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-//your code here!
-
+let box = document.querySelector("#infi-list")
+let count = 10;
+box.onscroll = ()=>{
+    if((box.clientHeight 
+            + box.scrollTop) >=
+            box.scrollHeight){
+                count++
+                let li1 = document.createElement("li")
+                let li2 = document.createElement("li")
+                li1.innerHTML = `Item ${count}`
+                box.appendChild(li1)
+                count++
+                li2.innerHTML = `Item ${count}`
+                box.appendChild(li2)
+            }
+        }
